a893c3826a76fe81988a3d00413a26015100249f
Matthew Jorgensen <matthew@jrgnsn.net>
2018-04-05 00:36:34 -0500

    Started on the bot

---
+++ bot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: urt-8 -*-
+
+from telegram.utils.helpers import escape_markdown
+
+from telegram import InlineQueryResultArticle, ParseMode, \
+        InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler, CommandHelper
+import logging
+
+# Enable logging
+logging.basicConfig(format='[%(asctime)s] - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+
