30a25ab23a579412e25e2baed8812b979e19fd94
Matthew Jorgensen <matthew@jrgnsn.net>
2018-04-14 12:58:16 -0500

    Finished writing db.py, now capable of connecting to out database

--- ext/db.py
+++ ext/db.py
@@ -1,9 +1,18 @@
 import sqlite3
 
 class Database:
-    def __init__(self):
-        self.conn =
-        self.pps = self.get_points(self.conn)
-    
-    def get_points(self):
-        
+    def __init__(self, database='data/db.sqlite3'):
+        self.database = database
+        self.conn = None
+
+    def connect(self):
+        """
+        Connects to the database and creates the `points` table if it doesn't
+        already exist.
+        """
+        self.conn = sqlite3.connect(self.database)
+        with self.conn as conn:
+            cur = conn.cursor()
+            cur.execute("""CREATE TABLE IF NOT EXISTS `points`
+                (key text NOT NULL UNIQUE, value int NOT NULL)""")
+        return self.conn
